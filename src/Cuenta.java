0717c92e76fa1199e80724b8b87ba941aeb11589
admin <javiervg01@gmail.com>
2023-02-03 18:44:44 +0100

    v1.2 FINAL

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,20 +1,27 @@
 import java.util.ArrayList;
 
-public class Cuenta extends Thread{
+public class Cuenta extends Thread {
     static int SALDO = 50;
     ArrayList<String> usuarios = new ArrayList<>();
 
-    public void run(){
+    public void run() {
         usuarios.add("Isabel");
         usuarios.add("Fernando");
-        while (SALDO >= 0) {
+        System.out.println("SALDO INICIAL: " + SALDO);
+        while (SALDO > 0) {
             retirarDinero();
         }
-
-
     }
-    public synchronized void retirarDinero(){
-        for (int i = 0; i < usuarios.size(); i++){
+
+    public synchronized void retirarDinero() {
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (SALDO == 0) {
+                break;
+            } else {
+                if (i == usuarios.size()) {
+                    i = 0;
+                }
+            }
             System.out.println(usuarios.get(i) + " va a retirar dinero");
             try {
                 sleep(500);
@@ -24,12 +31,9 @@ public class Cuenta extends Thread{
                 throw new RuntimeException(e);
             }
             System.out.println("SALDO ACTUAL: " + SALDO);
-
-            if (i == usuarios.size()){
-                i = 0;
-            }
             notify();
         }
 
     }
+
 }
